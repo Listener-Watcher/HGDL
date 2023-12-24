19e7dfa2893e37853720a9a590a26a143449d6dd
Listener-Watcher <philking492@gmail.com>
2023-12-23 22:59:09 -0500

    update

---
+++ main_transformer.py
@@ -0,0 +1,107 @@
+import torch
+from sklearn.metrics import f1_score
+#from utils import EarlyStopping, load_data
+from scipy.spatial import distance
+from utils_ import clark,intersection
+from utils_mugcn_pre import *
+from model import Gtransformerblock
+import dgl
+eps = 1e-9
+def score(logits, labels):
+    _, indices = torch.max(logits, dim=1)
+    _, indices2 = torch.max(labels,dim=1)
+    prediction = indices.long().cpu().numpy()
+    #labels = labels.cpu().numpy()
+    labels_ = indices.long().cpu().numpy()
+
+    accuracy = (prediction == labels_).sum() / len(prediction)
+    micro_f1 = f1_score(labels_, prediction, average="micro")
+    macro_f1 = f1_score(labels_, prediction, average="macro")
+    score_cosin = 0
+    score_can = 0
+    score_cheb = 0
+    for i in range(labels.shape[0]):
+        score_cosin += distance.cosine(labels[i].cpu().detach().numpy(),logits[i].cpu().detach().numpy())
+        score_can += distance.canberra(labels[i].cpu().detach().numpy(),logits[i].cpu().detach().numpy())
+        score_cheb += distance.chebyshev(labels[i].cpu().detach().numpy(),logits[i].cpu().detach().numpy())
+    score_cosin/= labels.shape[0]
+    score_can/=labels.shape[0]
+    score_cheb/=labels.shape[0]
+    score_clark = clark(logits+eps,labels+eps)
+    score_intersection = intersection(logits,labels)
+    print(f'cosin distance:{score_cosin:.4f}')
+    print(f'canberra distance:{score_can:.4f}')
+    print(f'chebyshev distance:{score_cheb:.4f}')
+    print(f'clark distance:{score_clark:.4f}')
+    print(f'intersection distance:{score_intersection:.4f}')
+    return accuracy, micro_f1, macro_f1
+
+
+def evaluate(model, g, features, labels, mask, loss_func):
+    model.eval()
+    with torch.no_grad():
+        logits = model(g, features)
+    loss = loss_func((logits[mask]+1e-9).log(), labels[mask]+1e-9)
+    accuracy, micro_f1, macro_f1 = score(logits[mask], labels[mask])
+
+    return loss, accuracy, micro_f1, macro_f1
+
+
+def main(args):
+    # If args['hetero'] is True, g would be a heterogeneous graph.
+    # Otherwise, it will be a list of homogeneous graphs.
+    (
+        g,
+        features,
+        labels,
+        num_classes,
+        train_idx,
+        val_idx,
+        test_idx,
+        train_mask,
+        val_mask,
+        test_mask,
+    ) = load_data(args.dataset)
+    #APCPA
+    meta_paths=[[("author","to","paper"),("paper","to","author")],[("author","to","paper"),("paper","to","conference"),("conference","to","paper"),("paper","to","author")]]
+    adj_list = []
+    for meta_path in meta_paths:
+        g1 = dgl.metapath_reachable_graph(g,meta_path)
+        A1 = g1.adj()
+        adj_list.append(A1)
+    print("A1",adj_list[0].shape)
+    print(features.shape)
+    num_heads = len(adj_list)
+    in_dim = features.shape[1]
+    out_dim = num_classes
+    dropout = args.dropout
+    layer_norm = args.layer_norm
+    model = Gtransformerblock(in_dim=in_dim, out_dim=out_dim, num_heads=num_heads, dropout=dropout, layer_norm=layer_norm, use_bias=use_bias)
+    print("begin test")
+    model.forward(adj_list,features)
+    print("end test")
+if __name__ == "__main__":
+    import argparse
+    #from utils import setup
+    parser = argparse.ArgumentParser("HAN")
+    parser.add_argument("-s", "--seed", type=int, default=1, help="Random seed")
+    """parser.add_argument(
+        "-ld",
+        "--log-dir",
+        type=str,
+        default="results",
+        help="Dir for saving training results",
+    )"""
+    parser.add_argument('--device', type=str, default='cuda:0' if torch.cuda.is_available() else 'cpu', help='gpu or cpu')
+    parser.add_argument('--epochs', type=int, default=6000)
+    parser.add_argument('--dataset',type=str,default='dblp')
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--alpha',type=float,default=0)
+    parser.add_argument('--weight_decay', type=float, default=0.0005)
+    parser.add_argument('--dropout', type=float, default=0.6)
+    parser.add_argument('--layer_norm',type=bool,default=True)
+    parser.add_argument('--residual',type=bool,default=True)
+    parser.add_argument('--use_bias',type=bool,default=True)
+    args = parser.parse_args()
+    args = setup(args)
+    main(args)
---
+++ model.py
@@ -0,0 +1,85 @@
+import dgl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dgl.nn.pytorch import GATConv
+import torch_geometric as geo
+import copy
+
+
+class MultiAttentionLayer(nn.Module):
+    def __init__(self, in_dim,out_dim,num_heads,use_bias):
+        super().__init__()
+        self.out_dim = out_dim
+        self.num_heads = num_heads
+        
+        if use_bias:
+            self.Q = nn.Linear(in_dim,out_dim*num_heads,bias=True)
+            self.K = nn.Linear(in_dim,out_dim*num_heads,bias=True)
+            self.V = nn.Linear(in_dim,out_dim*num_heads,bias=True)
+        else:
+            self.Q = nn.Linear(in_dim,out_dim*num_heads,bias=False)
+            self.K = nn.Linear(in_dim,out_dim*num_heads,bias=False)
+            self.V = nn.Linear(in_dim,out_dim*num_heads,bias=False)
+    def forward(self,adj_list,h):
+        Q_h = self.Q(h)
+        K_h = self.K(h)
+        V_h = self.V(h)
+        
+        A_tilde = torch.mul((1/torch.sqrt(h.shape[1]))*torch.torch.matmul(Q_h,torch.transpose(K_h,0,1)),adj_list[0])
+        print("A_tilde",A_tilde.shape)
+        attention = torch.nn.functional.softmax(A_tilde,dim=1)
+        print("attention",attention.shape)
+        print("V_h",V_h.shape)
+        X_tilde = torch.matmul(attention,V_h)
+        print("X_tilde",X_tilde.shape)
+        return X_tilde
+
+class Gtransformerblock(nn.Module):
+    def __init__(
+        self,in_dim, out_dim, num_heads, dropout=0.0, layer_norm=True, use_bias=False):
+        super().__init__()
+        self.in_channels = in_dim
+        self.out_channels = out_dim
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.layer_norm = layer_norm        
+        self.attentionlayer = MultiAttentionLayer(in_dim,out_dim//num_heads,num_heads,use_bias)
+        self.Q2 = nn.Linear(out_dim,out_dim,bias=True)
+        self.K2 = nn.Linear(out_dim,out_dim,bias=True)
+        self.V2 = nn.Linear(out_dim,out_dim,bias=True)
+        self.projection = nn.Linear(out_dim,out_dim)
+        if self.layer_norm:
+            self.layer_norm1 = nn.LayerNorm(out_dim)
+        self.FFN1 = nn.Linear(out_dim,out_dim*2)
+        self.FFN2 = nn.Linear(out_dim*2,out_dim)
+        if self.layer_norm:
+            self.layer_norm2 = nn.Layer_nrom(out_dim)
+
+    def forward(self, adj_list, h):
+        h_in1 = h
+        attn_out = self.attentionlayer(adj_list,h)
+        print("multi-head",attn_out.shape)
+        # for i in range(attn_out.shape[0]):
+        #     self.Q2(attn_out[i])
+        #     self.K2(attn_out[i])
+        #     self.V2(attn_out[i])
+        # h = attn_out.view(-1,self.out_channels)
+        # h = F.dropout(h,self.dropout,training=self.training)
+        # h = self.projection(h)
+        # if self.residual:
+        #     h = h_in1+h
+        # if self.layer_norm:
+        #     h = self.layer_norm1(h)
+        # h_in2 = h
+        # h = self.FFN1(h)
+        # h = F.relu(h)
+        # h = F.dropout(h,self.dropout,trianing=self.training)
+        # h = self.FFn2(h)
+        # if self.residual:
+        #     h = h_in2+h
+        # if self.layer_norm:
+        #     h = self.layer_norm2(h)
+        
+        # return F.softmax(self.predict(h),dim=1)
+        return attn_out
